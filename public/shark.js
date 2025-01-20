e8337671b82efc057bc3116a8fe2136be50453af
Iain Crawford (dakodun) <github@chikin.net>
2025-01-20 20:37:28 +0000

    these should have been ignored

--- public/shark.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  let shark = document.querySelector('#secretshark');
-  if (shark != null) {
-    let sharkNum = Math.floor(Math.random() * 2) + 1;
-    shark.src = '/site/secretshark_' + sharkNum + '.png';
-  }
-};
